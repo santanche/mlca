581de9b4e31316f537bd76b3343ffc048757237b
Roger <rogeriodeob@hotmail.com>
2014-05-09 15:40:11 -0300

    WorstMatrix implemented (for integers so far...)

---
+++ mlca_WorstMatrix.js
@@ -0,0 +1,35 @@
+﻿mlca.WorstMatrix = function (specs) {
+	'use strict';
+	
+	this.dimensions = specs.dimensions;
+	this.type = specs.type;
+	this.topology = specs.topology;
+	
+	this.matrix = [];
+	this.matrix.length = this.dimensions.x * this.dimensions.y;
+	
+	var i = 0;	// initialize the matrix
+	switch (this.type) {
+		case 'integer':	
+			for (i = 0; i < this.matrix.length; i += 1) {
+				this.matrix[i] = 0;
+			}
+		break;
+	}
+};
+
+mlca.WorstMatrix.prototype = new mlca.LayerDataStructure();
+
+mlca.WorstMatrix.prototype.getCell = function (coord) {
+	'use strict';
+	
+	return this.matrix[(coord.y * this.dimensions.x) + coord.x];
+};
+
+mlca.WorstMatrix.prototype.setCell = function (coord, newValue) {
+	'use strict';
+	
+	this.matrix[(coord.y * this.dimensions.x) + coord.x] = newValue;
+	
+	return;
+};
